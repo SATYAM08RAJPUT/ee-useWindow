a3a4323d08878dc2657eca2ace290b9e7d6a7b1e
Satyam Rajput <satyamrajput081998@gmail.com>
2024-12-02 06:32:45 -0500

    add height also

--- src/useWindow.jsx
+++ src/useWindow.jsx
@@ -2,12 +2,16 @@ import { useState,useEffect } from "react";
 
 const useWindowResize = () => {
     const [width,setWidth] = useState(window.innerWidth)
+    const [height,setHeight] = useState(window.innerHeight)
 
     useEffect(() => {
-        const handleResize  =  setWidth(window.innerWidth)
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+            setHeight(window.innerHeight)
+        } 
         window.addEventListener('resize',handleResize)
         return () => window.removeEventListener('resize',handleResize)
     },[])
-    return {width}
+    return {width,height}
 }
 export default useWindowResize
